a29a0706d68d95298bf204b1836bc9f9ee94a584
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-11 20:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function firstWord(s) {
+	
 	let x=s.trim();
 	let str="";
-	for(let i=0; i<s.length; i++){
-		if(s[i]==" "){
+	for(let i=0; i<x.length; i++){
+		if(x[i]==" "){
 			return str;
 		}
 		else{
-			str+=s[i];
+			str+=x[i];
 		}
 	}
 }
-
-
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
